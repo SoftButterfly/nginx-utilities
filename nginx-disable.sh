0a25fcca03372dd76371396a86ed4fa99bdf9698
ZodiacFireworks <martin.vuelta@gmail.com>
2016-06-05 09:50:42 -0500

    NGINX::Utilities
    Change log:
      * Added fist utilities: enable and disable sites
    
    Modified files:
      * new file:   nginx-disable.sh
      * new file:   nginx-enable.sh
      * new file:   nginx-functions.sh
      * new file:   nginx-variables.sh

---
+++ nginx-disable.sh
@@ -0,0 +1,238 @@
+#!/bin/bash
+# ngix-enable site
+# ------------------------------------------------------------------------------
+# NGINX::Utilities::Disable
+# ---------------------------
+# nginx-disable.sh script disables a site running under nginx
+#
+# --
+# See more at https://github.com/SoftButterfly/nginx-utilities
+#
+# --
+# @ZodiacFireworks (Martin Vuelta)
+# martin.vuelta@gmail.com
+
+# ------------------------------------------------------------------------------
+# Loading utilities
+
+source nginx-functions.sh
+source nginx-variables.sh
+
+
+# ------------------------------------------------------------------------------
+# Specific functions
+print_help () {
+    local __script_name="$1"
+    echo "$NGINX_COLLECTION_NAME $__script_name"
+    exit 0
+}
+
+
+# ------------------------------------------------------------------------------
+# Initialization
+
+__this_name="$(basename "$0" 2> /dev/null)"
+__this_argvs="$*"
+__this_argvl="$@"
+__this_gargs="$#"
+__this_min_rargs="2"
+
+sites_list=()
+files_list=()
+
+sites_path_list=()
+files_path_list=()
+
+
+# ------------------------------------------------------------------------------
+# Execution
+
+# Verification of user
+is_run_as_root $__this_name
+
+# Correct number of args
+if [[ "$__this_gargs" -ne "1" ]]; then
+    is_script_run_whit_wrong_nargs "$__this_name" "$__this_min_rargs" "$__this_gargs"
+fi
+
+
+if [[ "$__this_gargs" -eq "1" ]]; then
+    if [[ "$1" == "$HELP_SH_FLAG" || "$1" == "$HELP_LG_FLAG" ]]; then
+        print_help
+
+    elif [[ "$1" == "$FILE_SH_FLAG" || "$1" == "$FILE_LG_FLAG" ]]; then
+        orphan_flag "$__this_name" "$1"
+
+    elif [[ "$1" == "$SITE_SH_FLAG" || "$1" == "$SITE_LG_FLAG" ]]; then
+        orphan_flag "$__this_name" "$1"
+    fi
+
+    sites_list[1]="$1"
+
+else
+    __fc=$((1))
+    __sc=$((1))
+
+    while [[ "$#" -gt "1" || "$#" -eq "1" ]]; do
+        __flag="$1"
+
+        case $__flag in
+            $FILE_SH_FLAG|$FILE_LG_FLAG)
+                __value="$2"
+
+                if [[ -z "$__value" ]]; then
+                    orphan_flag "$__this_name" "$__flag"
+                fi
+
+                files_list[__fc]="$__value"
+                __fc=$(($__fc + 1))
+
+                shift
+            ;;
+
+            $SITE_SH_FLAG|$SITE_LG_FLAG)
+                __value="$2"
+
+                if [[ -z "$__value" ]]; then
+                    orphan_flag "$__this_name" "$__flag"
+                fi
+
+                sites_list[__sc]="$__value"
+                __sc=$(($__sc + 1))
+
+                shift
+            ;;
+
+            $HELP_SH_FLAG|$HELP_LG_FLAG)
+                print_help $__this_name
+
+                shift
+            ;;
+
+            *)
+                is_unknown_flag "$__this_name" "$__flag"
+
+                shift
+            ;;
+        esac
+
+        shift
+    done
+fi
+
+
+__fc=$((1))
+for file_name in ${files_list[@]}; do
+    files_path_list[__fc]=$(get_absolute_path $file_name)
+    files_list[__fc]="$(basename "${files_path_list[$__fc]}" 2> /dev/null)"
+    __fc=$(($__fc + 1))
+done
+
+
+__sc=$((1))
+for site_name in ${sites_list[@]}; do
+    sites_path_list[__sc]=$(get_site_enabled_config_file_path $site_name)
+    __sc=$(($__sc + 1))
+done
+
+
+__sc=$((1))
+for site_path in ${sites_path_list[@]}; do
+    site_name="${sites_list[$__sc]}"
+
+    if is_directory $site_path; then
+        >&2 echo "$NGINX_CODE_PREFIX_WARNING"
+        >&2 echo "Script  : $__this_name."
+        >&2 echo "Site    : $site_path"
+        >&2 echo "Message : Skkiping becuase provided path is a directroy."
+        >&2 echo ""
+
+    elif is_file $site_path; then
+        __yes_delete="n"
+
+        if !(is_sym_link $site_path); then
+            >&2 echo "$NGINX_CODE_PREFIX_WARNING"
+            >&2 echo "Script  : $__this_name."
+            >&2 echo "Site    : $site_name"
+            >&2 echo "Message : Provided path is not a symlink."
+            >&2 echo -n "Do you want disable this site [y/N]? "
+            read __yes_delete;
+
+            if [[ "$__yes_delete" == "y" || "$__yes_delete" == "Y" ]]; then
+                echo -n "* Disabling site '$site_name' ... "
+                rm -f $site_path
+                echo "Ok."
+                echo ""
+            else
+                echo "* Skipping site '$site_name'"
+                echo ""
+            fi
+        else
+            echo -n "* Disabling site '$site_name' ... "
+            rm -f $site_path
+            echo "Ok."
+            echo ""
+        fi
+
+    else
+        >&2 echo "$NGINX_CODE_PREFIX_WARNING"
+        >&2 echo "Script  : $__this_name."
+        >&2 echo "File    : $site_name"
+        >&2 echo "Message : Skkiping becuase provided path doesn't exist."
+        >&2 echo ""
+    fi
+
+    __sc=$(($__sc + 1))
+done
+
+
+__fc=$((1))
+for file_path in ${files_path_list[@]}; do
+    file_name="${files_list[$__fc]}"
+
+    if is_directory $file_path; then
+        >&2 echo "$NGINX_CODE_PREFIX_WARNING"
+        >&2 echo "Script  : $__this_name."
+        >&2 echo "File    : $file_name"
+        >&2 echo "Message : Skkiping becuase provided path is a directroy."
+        >&2 echo ""
+
+    elif is_file $file_path; then
+        __yes_delete="n"
+
+        if !(is_sym_link $file_path); then
+            >&2 echo "$NGINX_CODE_PREFIX_WARNING"
+            >&2 echo "Script  : $__this_name."
+            >&2 echo "File    : $file_name"
+            >&2 echo "Message : Provided path is not a symlink."
+            >&2 echo -n "Do you want disable this site [y/N]? "
+            read __yes_delete;
+
+            if [[ "$__yes_delete" == "y" || "$__yes_delete" == "Y" ]]; then
+                echo -n "* Disabling file '$file_name' ... "
+                rm -f $file_path
+                echo "Ok."
+                echo ""
+            else
+                echo "* Skipping file '$file_name'"
+                echo ""
+            fi
+        else
+            echo -n "* Disabling file '$file_name' ... "
+            rm -f $file_path
+            echo "Ok."
+            echo ""
+        fi
+
+    else
+        >&2 echo "$NGINX_CODE_PREFIX_WARNING"
+        >&2 echo "Script  : $__this_name."
+        >&2 echo "File    : $file_path"
+        >&2 echo "Message : Skkiping becuase provided path doesn't exist."
+        >&2 echo ""
+    fi
+
+    __fc=$(($__fc + 1))
+done
+
+nginx -s reload
---
+++ nginx-functions.sh
@@ -0,0 +1,439 @@
+#!/bin/bash
+# ------------------------------------------------------------------------------
+# NGINX::Utilities::Functions
+# ---------------------------
+# nginx-functions.sh script contains useful and reusable functions for other
+# scripts in this NGINX::Utilities Scripts Collection
+#
+# --
+# See more at https://github.com/SoftButterfly/nginx-utilities
+#
+# --
+# @ZodiacFireworks (Martin Vuelta)
+# martin.vuelta@gmail.com
+
+# ------------------------------------------------------------------------------
+# Loading utilities
+
+source nginx-variables.sh
+
+
+# ------------------------------------------------------------------------------
+# Prevent Execution
+
+if [[ -n $BASH_VERSION ]] && [[ "$(basename "$0" 2> /dev/null)" == "nginx-functions.sh" ]]; then
+    >&2 echo $NGINX_CODE_PREFIX_ERROR
+    >&2 echo "File    : $(basename "$0" 2> /dev/null)"
+    >&2 echo "Message : This script is intended to sourced inside of other scripts in"
+    >&2 echo "          NGINX::Utilities Scripts Collection. Not to be executed as"
+    >&2 echo "          independent part."
+    >&2 echo ""
+
+    exit 1
+fi
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   is_run_as_root ()
+# Arguments   :
+#   * $1 (script_name) -> script from this function is called.
+# Description :
+#   Verify if script is running as root user.
+
+is_run_as_root () {
+    local __script_name="$1"
+
+    if [ "$(id -u)" != "0" ]; then
+        >&2 echo $NGINX_CODE_PREFIX_ERROR
+
+        if [ -n "$__script_name" ]; then
+            >&2 echo "File    : $__script_name"
+        fi
+
+        >&2 echo "Message : This script must be run as root."
+        >&2 echo ""
+
+        exit 1
+    fi
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   is_function_run_whit_no_arguments ()
+# Arguments   :
+#   * $1 (function_name) -> function from this function is called.
+#   * $2 (function_rargs) -> number of required arguments to function_name.
+#   * $3 (function_gargs) -> number of given arguments to function_name.
+# Description :
+#   Verify if function is running whit the correct number of arguments
+
+is_function_run_whit_wrong_nargs () {
+    local __this_name="is_function_run_whit_wrong_nargs"
+    local __this_rargs=3
+    local __this_gargs="$#"
+
+    if [ "$__this_rargs" != "$__this_gargs" ]; then
+        is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    else
+        local __function_name="$1"
+        local __function_rargs="$2"
+        local __function_gargs="$3"
+
+        if [ "$__function_rargs" -ne "$__function_gargs" ]; then
+            if [ -z "$__function_name" ]; then
+                local __function_name="Unknown function"
+            fi
+
+            >&2 echo "$NGINX_CODE_PREFIX_ERROR"
+            >&2 echo "Function : $__function_name."
+            >&2 echo "Message  : This function was executed with $__function_gargs arguments but $__function_rargs are required."
+            >&2 echo ""
+
+            exit 1
+        fi
+    fi
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   is_script_run_whit_no_arguments ()
+# Arguments   :
+#   * $1 (script_name) -> script from this function is called.
+#   * $2 (script_min_rargs) -> minimum number of required arguments to
+#     script_name.
+#   * $3 (script_gargs) -> number of given arguments to script_name.
+# Description :
+#   Verify if script is running without arguments
+
+is_script_run_whit_wrong_nargs () {
+    local __this_name="is_script_run_whit_wrong_nargs"
+    local __this_rargs="3"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __script_name="$1"
+    local __script_min_rargs="$2"
+    local __script_gargs="$3"
+
+    if [ "$__script_gargs" -lt "$__script_min_rargs" ]; then
+        if [ -z "$__script_name" ]; then
+            local __script_name="unknown_script"
+        fi
+
+        >&2 echo "$NGINX_CODE_PREFIX_ERROR"
+        >&2 echo "Script  : $__script_name."
+        >&2 echo "Message : This function was executed with $__script_gargs arguments but at least"
+        >&2 echo "          $__script_min_rargs are required."
+        >&2 echo "          Please use '$__script_name $HELP_SH_FLAG|$HELP_LG_FLAG' to check usage."
+
+        exit 1
+    fi
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   is_unknown_flag ()
+# Arguments   :
+#   * $1 (script_name) -> script from this function is called.
+#   * $2 (unknown_flag) -> flag recognized as unknown.
+# Description :
+#   Print unknown flag error message
+
+is_unknown_flag () {
+    local __this_name="is_unknown_flag"
+    local __this_rargs="2"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __script_name="$1"
+    local __unknown_flag="$2"
+
+    if [ -z "$__script_name" ]; then
+        local __script_name="unknown_script"
+    fi
+
+    if [ -z "$__unknown_flag" ]; then
+        local __unknown_flag="unknown_flag"
+    fi
+
+    >&2 echo "$NGINX_CODE_PREFIX_ERROR"
+    >&2 echo "Script  : $__script_name."
+    >&2 echo "Message : Unknown flag ('$__unknown_flag') provided"
+    >&2 echo "          Please use '$__script_name $HELP_SH_FLAG|$HELP_LG_FLAG' to check usage."
+
+    exit 1
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   orphan_flag ()
+# Arguments   :
+#   * $1 (script_name) -> script from this function is called.
+#   * $2 (orphan_flag) -> flag recognized as unknown.
+# Description :
+#   Print unknown flag error message
+
+orphan_flag () {
+    local __this_name="is_unknown_flag"
+    local __this_rargs="2"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __script_name="$1"
+    local __orphan_flag="$2"
+
+    if [ -z "$__script_name" ]; then
+        local __script_name="unknown_script"
+    fi
+
+    if [ -z "$__orphan_flag" ]; then
+        local __orphan_flag="unknown_flag"
+    fi
+
+    >&2 echo "$NGINX_CODE_PREFIX_ERROR"
+    >&2 echo "Script  : $__script_name."
+    >&2 echo "Message : This flag ('$__orphan_flag') needs one argument to be used."
+    >&2 echo "          Please use '$__script_name $HELP_SH_FLAG|$HELP_LG_FLAG' to check usage."
+
+    exit 1
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   get_directory_name ()
+# Arguments   :
+#   * $1 (relative_dof_path) -> directory or file path.
+# Description :
+#   Return the absolute path for the container directory of a given file or
+#   directory path.
+# Notes:
+#   * dof stands for "directory or file"
+
+get_directory_name () {
+    local __this_name="get_directory_name"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __relative_dof_path="$1"
+
+    echo "$(cd "$(dirname "$__relative_dof_path")" && pwd)"
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   get_absolute_path ()
+# Arguments   :
+#   * $1 (relative_dof_path) -> directory or file path.
+# Description :
+#   Return the absolute path for a given path
+# Notes:
+#   * dof stands for "Directory Or File"
+
+get_absolute_path () {
+    local __this_name="get_absolute_path"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __relative_dof_path="$1"
+
+    local __dir_name="$(get_directory_name "$__relative_dof_path")"
+    local __dof_name="$(basename "$__relative_dof_path" 2> /dev/null)"
+
+    if [[ "${__dir_name: -1}" != "/" ]]; then
+        local __dir_name="$__dir_name""/"
+    fi
+
+    echo "$__dir_name""$__dof_name"
+}
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   get_file_name ()
+# Arguments   :
+#   * $1 (file_path) -> file path.
+# Description :
+#   Return the name without extension of a given file
+
+get_file_name () {
+    local __this_name="get_file_name"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __file_path="$1"
+    local __absolute_path="$(get_absolute_path $__file_path)"
+
+    local __file_name="$(basename "$__absolute_path" 2> /dev/null)"
+
+    local __file_name="${__file_name%.*}"
+
+    echo "$__file_name"
+}
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   get_file_extension ()
+# Arguments   :
+#   * $1 (file_path) -> file path.
+# Description :
+#   Return the extension of a given file
+
+get_file_extension () {
+    local __this_name="get_file_extensions"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __file_path="$1"
+    local __absolute_path="$(get_absolute_path $__file_path)"
+
+    local __file_name="$(basename "$__absolute_path" 2> /dev/null)"
+
+    local __file_extension="${__file_name##*.}"
+
+    echo "$__file_extension"
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   get_site_enabled_config_file_path ()
+# Arguments   :
+#   * $1 (site_name) -> nginx site name.
+# Description :
+#   Return the absolute path to the config file of site_name
+
+get_site_enabled_config_file_path () {
+    local __this_name="get_site_enabled_config_file_path"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __site_name="$1.conf"
+    local __dir_name=$NGINX_ENABLED_SITES_DIRECTORY
+
+    if [[ "${__dir_name: -1}" != "/" ]]; then
+        local __dir_name="$__dir_name""/"
+    fi
+
+    echo "$__dir_name""$__site_name"
+}
+
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   get_site_available_config_file_path ()
+# Arguments   :
+#   * $1 (site_name) -> nginx site name.
+# Description :
+#   Return the absolute path to the config file of site_name
+
+get_site_available_config_file_path () {
+    local __this_name="get_site_available_config_file_path"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __site_name="$1.conf"
+    local __dir_name=$NGINX_AVAILABLE_SITES_DIRECTORY
+
+    if [[ "${__dir_name: -1}" != "/" ]]; then
+        local __dir_name="$__dir_name""/"
+    fi
+
+    echo "$__dir_name""$__site_name"
+}
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   is_directory ()
+# Arguments   :
+#   * $1 (dir_name) -> directory path.
+# Description :
+#   Checks if the given path is a directory
+
+is_directory () {
+    local __this_name="is_directory"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __dir_name="$1"
+    local __dir_path="$(get_absolute_path "$__dir_name")"
+
+    if [[ -d "$__dir_path" ]];  then
+        true
+    else
+        false
+    fi
+}
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   is_file ()
+# Arguments   :
+#   * $1 (file_name) -> file path.
+# Description :
+#   Checks if the given path is a file.
+
+is_file () {
+    local __this_name="is_file"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __file_name="$1"
+    local __file_path="$(get_absolute_path "$__file_name")"
+
+    if [[ -f "$__file_path" ]]; then
+        true
+    else
+        false
+    fi
+}
+
+# ------------------------------------------------------------------------------
+# Function    :
+#   is_sym_link ()
+# Arguments   :
+#   * $1 (sl_name) -> symbolic link path.
+# Description :
+#   Checks if the given path is a symbolic link.
+
+is_sym_link () {
+    local __this_name="is_sym_link"
+    local __this_rargs="1"
+    local __this_gargs="$#"
+
+    is_function_run_whit_wrong_nargs "$__this_name" "$__this_rargs" "$__this_gargs"
+
+    local __sl_name="$1"
+    local __sl_path="$(get_absolute_path "$__sl_name")"
+
+    if [[ -h "$__sl_path" ]]; then
+        true
+    else
+        false
+    fi
+}
